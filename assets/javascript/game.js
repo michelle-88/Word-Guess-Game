393cf0badfb4546f54cf3d9cea8b1e4763ff274c
Michelle M <53539385+michelle-88@users.noreply.github.com>
2019-08-27 20:04:31 -0400

    declared function that will be called whenever the player releases a pressed key. created empty array to store player's guessed letters. wrote logic to push player's guess into said array and display array on screen.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,5 +9,19 @@ var directionsText = document.getElementById("directions-text");
 var winsText = document.getElementById("wins-text");
 var currentWord = document.getElementById("currentword-text");
 var guessesRemain = document.getElementById("guessesremain-text");
+var letterGuessArray = [];
 var letterGuess = document.getElementById("letterguess-text");
 
+// Create function that will run whenever the player presses a key
+document.onkeyup = function(event){
+
+    // Determine which key was pressed
+    var userGuess = event.key;
+
+    // Push pressed key value into letterGuessArray
+    letterGuessArray.push(userGuess);
+
+    // Display letterGuessArray in letterGuess span in HTML
+    letterGuess.textContent = letterGuessArray;
+
+}

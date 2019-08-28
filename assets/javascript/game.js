090c909d96508eae9d87f12ebc91b8cc2790d02a
Michelle M <53539385+michelle-88@users.noreply.github.com>
2019-08-27 21:27:20 -0400

    added pseudocode to outline what JavaScript  needs to be coded for the remaining parts of the game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -30,9 +30,24 @@ document.onkeyup = function(event){
     letterGuessArray.push(userGuess);
 
     // Display letterGuessArray in letterGuess span in HTML
-    letterGuess.textContent = letterGuessArray;
+    letterGuess.textContent = letterGuessArray
 
 
+    
 
 
 }
+
+// Have browser display dashes in place of each letter in Current Word (for loop?)
+// Write logic to determine is key pressed by player matches any letter in Current Word
+    // if the pressed key matches, display letter in Current Word
+    // if the pressed key does not match, decrement number of guesses remaining and display the letter in Letters Already Guessed (need an array for this)
+// Write logic to track keys already pressed and make them unselectable again (if letter is in Letters Already Guessed array, make them unresponsive?)
+// Write logic to determine when player wins - player needs to press every key in Current Word before they run out of guesses
+    // If player wins, display picture of Star Wars character/word, display guessed word in heading, & increment Wins score
+    // If player wins, I will also need to reset Current Word, Guesses Remaining, & Letters Already Guessed
+// Write logic to determine when player loses - when they run out of guesses before pressing every key in Current Word
+    // If player loses, reset everything
+
+
+

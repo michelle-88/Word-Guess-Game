daf87351dde0128f62c4bbe8126df378b3d40196
Michelle M <53539385+michelle-88@users.noreply.github.com>
2019-08-27 20:23:29 -0400

    added words to wordChoices array. wrote logic to randomly choose a word from this array before the game starts (before player presses key). targeted span next to Current Word in HTML and made it display randomly chosen word.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // Create an array that contains all the possible words for the game
-var wordChoices = [];
+var wordChoices = ["Darth Vader", "Skywalker", "Lightsaber", "Droid", "Yoda", "Jedi", "Sith"];
 
 // Create variable to store number of player wins. This starts at 0.
 var wins = 0;
@@ -7,10 +7,18 @@ var wins = 0;
 // Create variables to store all the locations in the HTML where the textContent will be updated accordingly to player input.
 var directionsText = document.getElementById("directions-text");
 var winsText = document.getElementById("wins-text");
-var currentWord = document.getElementById("currentword-text");
-var guessesRemain = document.getElementById("guessesremain-text");
+var currentWordText = document.getElementById("currentword-text");
+var guessesRemainText = document.getElementById("guessesremain-text");
 var letterGuessArray = [];
 var letterGuess = document.getElementById("letterguess-text");
+
+
+// Randomly choose a word from the wordChoices array
+var currentWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+
+// Display randomly chosen word in "Current Word" field in html
+currentWordText.textContent = currentWord;
+
 
 // Create function that will run whenever the player presses a key
 document.onkeyup = function(event){
@@ -23,5 +31,8 @@ document.onkeyup = function(event){
 
     // Display letterGuessArray in letterGuess span in HTML
     letterGuess.textContent = letterGuessArray;
+
+
+
 
 }
